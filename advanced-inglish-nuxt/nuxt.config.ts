5fbb1700fe567a86781d6965131786e5eef8d71a
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-04-08 10:10:41 +0700

    Initial commit for Nuxt version of Advanced Inglish

---
+++ advanced-inglish-nuxt/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ]
+})
