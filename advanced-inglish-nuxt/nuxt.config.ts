7621ff428e8ffc72f5069f6ede428462d083bfe1
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-04-08 13:36:29 +0700

    Updated some necessary libraries + added some code snippets

--- advanced-inglish-nuxt/nuxt.config.ts
+++ advanced-inglish-nuxt/nuxt.config.ts
@@ -1,16 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/ui',
-    '@nuxt/test-utils',
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/ui",
+    "@nuxt/test-utils",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+  ],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ advanced-inglish-nuxt/pages/demo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-3xl"><h1>This is the Demo Page</h1></div>
+</template>
+
+<script setup></script>
