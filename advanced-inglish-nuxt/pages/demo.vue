c2cc535ae9e7cd7530b0dab0b9a3d52820877fc3
Mingu <vhnm3004@outlook.com>
2025-04-08 22:59:59 +0700

    Added + updated tailwind config file in order for intellisense to work

---
+++ advanced-inglish-nuxt/pages/demo.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="bg-pink-300">
+		<h1 class="text-3xl">This is the demo page</h1>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ advanced-inglish-nuxt/tailwind.config.ts
@@ -0,0 +1,3 @@
+export default {
+	content: ["./pages/**/*.{vue,js,ts,jsx,tsx}", "./index.html"],
+};
