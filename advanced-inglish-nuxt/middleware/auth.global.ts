a007f08f6fac5121e1850c4ca396f7a54bc4404c
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-04-18 14:21:48 +0700

    Added some notes for auth middleware utilization

--- advanced-inglish-nuxt/middleware/auth.global.ts
+++ advanced-inglish-nuxt/middleware/auth.global.ts
@@ -1,13 +1,22 @@
 import { useAuth } from "~/composables/auth/useAuth";
-export default defineNuxtRouteMiddleware((to, from) => {
+
+export default defineNuxtRouteMiddleware((to) => {
   const { isAuthenticated } = useAuth();
 
-  if (
-    !isAuthenticated.value &&
-    to.path !== "/login" &&
-    to.path !== "/register" &&
-    to.path !== "/"
-  ) {
-    return navigateTo("/login");
+  // Check if route requires authentication
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    // Store original destination for redirect after login
+    return navigateTo({
+      path: "/login",
+      query: { redirect: to.fullPath },
+    });
   }
 });
+
+//Note: This middleware checks if the route requires authentication and if the user is authenticated. If not, it redirects to the login page and stores the original destination in the query parameters for redirection after successful login.
+/*How to implement: Go to pages that need protection and add the following code to the top of the script section:
+ <script setup>
+definePageMeta({
+  requiresAuth: true
+})
+</script>*/
--- advanced-inglish-nuxt/utils/types/user.ts
+++ advanced-inglish-nuxt/utils/types/user.ts
@@ -11,8 +11,3 @@ export interface AuthResponse extends User {
   accessToken: string;
   refreshToken: string;
 }
-export interface TokenResponse {
-  accessToken: string;
-  refreshToken: string;
-  user: User;
-}
