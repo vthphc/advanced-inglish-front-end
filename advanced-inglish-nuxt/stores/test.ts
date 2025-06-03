12c0889495082d8e7faff6bb57fad8dbcfdf7c2c
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-06-03 16:55:29 +0700

    Checkpoint, re-implementing [parts].vue

--- advanced-inglish-nuxt/stores/test.ts
+++ advanced-inglish-nuxt/stores/test.ts
@@ -1,16 +1,30 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 import type { PopulatedTest } from "~/utils/types/test";
 
-export const useTestStore = defineStore("test", {
-    state: () => ({
-        currentTest: null as PopulatedTest | null,
-    }),
-    actions: {
-        setCurrentTest(test: PopulatedTest) {
-            this.currentTest = test;
-        },
-        clearCurrentTest() {
-            this.currentTest = null;
-        },
-    },
+export const useTestStore = defineStore("test", () => {
+    // State
+    const currentTest = ref<PopulatedTest | null>(null);
+
+    // Getters
+    const getCurrentTest = computed(() => currentTest.value);
+
+    // Actions
+    const setCurrentTest = (test: PopulatedTest) => {
+        currentTest.value = test;
+    };
+
+    const clearCurrentTest = () => {
+        currentTest.value = null;
+    };
+
+    return {
+        // State
+        currentTest,
+        // Getters
+        getCurrentTest,
+        // Actions
+        setCurrentTest,
+        clearCurrentTest,
+    };
 });
