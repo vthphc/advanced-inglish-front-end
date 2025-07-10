8453c8b19ec059ad7980d5f37279b18a231b678f
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-07-10 17:05:52 +0700

    Small update to default api path if not provided

--- advanced-inglish-nuxt/composables/api/useApi.ts
+++ advanced-inglish-nuxt/composables/api/useApi.ts
@@ -4,7 +4,10 @@ import { useRuntimeConfig } from "#app";
 
 export const useApi = () => {
     const config = useRuntimeConfig();
-    const baseURL = config.public.apiBase + "/api";
+    const baseURL =
+        (config.public.apiBase
+            ? config.public.apiBase
+            : "http://localhost:5000") + "/api";
 
     const api = axios.create({
         baseURL,
